b41e16c2dcc3bf781fb0490c2282deb95a18c142
dawninthemoon <39222540+dawninthemoon@users.noreply.github.com>
2025-02-13 22:30:00 +0900

    이펙트 베이스 추가

--- Assets/Scripts/Combat/Effects/EffectBase.cs
+++ Assets/Scripts/Combat/Effects/EffectBase.cs
@@ -6,6 +6,37 @@ namespace Combat
 {
     public class EffectBase : MonoBehaviour
     {
-        
+        [SerializeField]
+        public bool autoRelease;
+
+        protected CombatManager _combatManager;
+        protected System.Action _onDestroy;
+        protected bool _initialized;
+
+        public virtual void Init(System.Action onDestroy = null)
+        {
+            _onDestroy = onDestroy;
+            _initialized = true;
+        }
+
+        public void SetDependency(CombatManager combatManager)
+        {
+            _combatManager = combatManager;
+        }
+
+        private void Update()
+        {
+            OnUpdate();
+        }
+
+        protected virtual void OnUpdate()
+        {
+
+        }
+
+        public void Release()
+        {
+            gameObject.SetActive(false);
+        }
     }
 }
